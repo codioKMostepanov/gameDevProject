238da2b8aaa5fe058c74484c67d103d717e499c1
codioKMostepanov <kmostepanov@codio.com>
2024-12-04 20:50:36 +0000

    Added player choice feature.

--- gameProject/main.py
+++ gameProject/main.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
 print("We will bring you something exciting, soon!")
 print("Enjoy your adventure!")
 print("Conflicting feature in welcome-feature branch")
-
+print("Please choose your player.")
